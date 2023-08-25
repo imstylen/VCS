57f73bdfca0c4892bc4e03a5235c64db2f108a78
Douglas Monsky <douglas.monsky@gmail.com>
2023-08-25 16:37:01 -0400

    feat: refactor File class in file.py (#master)
    The File class in file.py has been significantly refactored. The
    previous implementation was removed and replaced with a more
    simplified version. The new implementation uses pathlib for file
    handling instead of os and shutil. The constructor now only takes a
    file_path argument, which can be a string or a Path object. A
    __str__ method has been added to return the file path as a string.
    A property method 'last_updated' has been added to return the last
    modified time of the file. The backup and check methods, along with
    several imports, have been removed.

--- source/file.py
+++ source/file.py
@@ -1,38 +1,13 @@
-import os
-import datetime
-import shutil
+from pathlib import Path
 
-from app import Application
 
 class File:
-    
-    def __init__(self, in_app:Application, in_path:str):
-        self.app = in_app
-        self.file_path:str = in_path
-        self.m_time = None
-        
-    def check(self):
-        new_m_time = os.path.getmtime(self.file_path)
-        
-        if self.m_time:
-            if new_m_time > self.m_time:
-                self.m_time = new_m_time
-                self.backup()
-        else:
-            self.m_time = new_m_time
-            self.backup()
-                
-    def backup(self):
-        print("Backing up: " + self.file_path)
-        
-        path_part_list = self.file_path.split(".")
-        
-        backup_path = self.app.backup_folder + "/" + path_part_list[:-1] + self.get_time_string() + "." + path_part_list[-1]
-        print(backup_path)
-        
-        #use shutil to copy
-    
-    def get_time_string() -> str:
-        current_datetime = datetime.datetime.now()
-        desired_format = current_datetime.strftime("%Y%m%d%H%M")
-        return desired_format
+    def __init__(self, file_path: str | Path) -> None:
+        self.file_path: Path = Path(file_path)
+
+    def __str__(self) -> str:
+        return str(self.file_path)
+
+    @property
+    def last_updated(self) -> float:
+        return self.file_path.stat().st_mtime
