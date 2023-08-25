64a3ff7299ef7977ee00a3b21d8d693cde7a3f10
Douglas Monsky <douglas.monsky@gmail.com>
2023-08-25 17:21:26 -0400

    feat: update path handling and logging in main.py (#99bb678)
    The main.py file has been updated to improve path handling and logging.
    The default relative path has been changed to be relative to the current
    script's directory. A new function, get_home_relative_path, has been added
    to get the home directory relative path. The create_working_folder function
    has been updated to use the new default path and to log the path being used.
    The main function has been updated to handle the app_path argument as a Path
    or str, and to use the updated create_working_folder function. The script's
    execution when run as the main module has been updated to use the new default
    path.

--- source/main.py
+++ source/main.py
@@ -3,27 +3,38 @@ from pathlib import Path
 
 from app import Application
 
-DEFAULT_RELATIVE_PATH = 'WorkingFolder'
 logging.basicConfig(level=logging.INFO)
 
+# Set the default path relative to the current script's directory, as to use the directory its contained within.
+CURRENT_DIRECTORY = Path(__file__).parent.parent
+DEFAULT_WORKINGFOLDER_PATH = CURRENT_DIRECTORY / 'WorkingFolder'
 
-def create_working_folder(path: Path | str = None) -> Path:
+
+def get_home_relative_path() -> Path:
+    return Path.home() / 'WorkingFolder'
+
+
+def create_working_folder(path: Path = None) -> Path:
     if path is None:
-        home_directory = Path.home()
-        path = Path(home_directory / DEFAULT_RELATIVE_PATH)
+        path = DEFAULT_WORKINGFOLDER_PATH
+        logging.info("Using default path: %s", path)
     path.mkdir(exist_ok=True)
     return path
 
 
-def main(app: Path | str = None) -> None:
+def main(app_path: Path | str = None) -> None:
     logging.info("Starting application")
+    app_path = Path(app_path) if app_path else None
     # Creates the working folder if it doesn't exist, continues if it does.
-    app_path = create_working_folder(app)
+    app_path = create_working_folder(app_path)
     Application(app_path)
     logging.info("Closing application")
 
 
 if __name__ == "__main__":
-    home_directory = Path.home()
-    target_directory = Path(home_directory / 'WorkingFolder')
-    main(target_directory)
+    # This will use the script's relative path by default.
+    main()
+
+    # If you want to use the home directory relative path, you can call:
+    # home_path = get_home_relative_path()
+    # main(home_path)
