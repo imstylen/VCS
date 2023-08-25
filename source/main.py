99bb678b39861811130449dfba74443b9d4e2ca2
Douglas Monsky <douglas.monsky@gmail.com>
2023-08-25 16:38:47 -0400

    feat(main.py): add logging and working folder creation functionality
    The main.py file has been updated to include logging functionality and
    the ability to create a working folder if it doesn't exist. The main
    function now accepts an optional path argument, which is used to create
    the working folder. If no path is provided, a default path is used.
    The Application instance is now created with the path to the working
    folder. The logging level has been set to INFO.

--- source/main.py
+++ source/main.py
@@ -1,11 +1,29 @@
+import logging
+from pathlib import Path
+
 from app import Application
 
-def main():
-    pass
+DEFAULT_RELATIVE_PATH = 'WorkingFolder'
+logging.basicConfig(level=logging.INFO)
+
+
+def create_working_folder(path: Path | str = None) -> Path:
+    if path is None:
+        home_directory = Path.home()
+        path = Path(home_directory / DEFAULT_RELATIVE_PATH)
+    path.mkdir(exist_ok=True)
+    return path
+
+
+def main(app: Path | str = None) -> None:
+    logging.info("Starting application")
+    # Creates the working folder if it doesn't exist, continues if it does.
+    app_path = create_working_folder(app)
+    Application(app_path)
+    logging.info("Closing application")
+
 
 if __name__ == "__main__":
-    print("Starting application")
-    
-    app = Application("WorkingFolder")
-    
-    print("Closing application")
+    home_directory = Path.home()
+    target_directory = Path(home_directory / 'WorkingFolder')
+    main(target_directory)
