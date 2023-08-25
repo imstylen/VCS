f1eafb325b7174cd3796410508d6358d44b9b658
Douglas Monsky <douglas.monsky@gmail.com>
2023-08-25 17:21:10 -0400

    feat: enhance file handling and backup process/ refactor file related logic from Application to FileHandler.
    This commit introduces significant improvements to the file handling and
    backup process in the 'file_handler.py' and 'app.py' files.
    
    In 'file_handler.py', new methods for adding files and performing backups
    have been added. The backup process now includes error handling for
    FileNotFoundError, PermissionError, and IOError.
    
    In 'app.py', the backup folder setup has been refactored into a private
    method. The update method now processes directories before files, and
    the addition of files and backups is now handled by the 'file_handler'.
    This commit also introduces methods for getting directories and files
    from a given path.
    
    These changes enhance the robustness and efficiency of the file handling
    and backup process.

--- source/app.py
+++ source/app.py
@@ -1,58 +1,46 @@
-import logging
-import shutil
 from pathlib import Path
+from typing import Iterator
 
-from file import File
 from source.file_handler import FileHandler
-from utils import get_current_time_str
 
 
 class Application:
     def __init__(self, in_directory: str) -> None:
         self.directory: Path = Path(in_directory)
-        self.backup_folder: Path = self.setup_backup_folder()
+        self.backup_folder: Path = self._setup_backup_folder()
         self.file_handler = FileHandler(self.backup_folder)
 
-        self.files: set[File] = set()
         self.update()
 
-    def setup_backup_folder(self) -> Path:
+    def __repr__(self) -> str:
+        return f"Application({self.directory})"
+
+    def update(self) -> None:
+        self._process_directory(self.directory)
+
+    def _setup_backup_folder(self) -> Path:
         backup_folder: Path = self.directory / ".backup"
         backup_folder.mkdir(exist_ok=True)
 
         return backup_folder
 
-    def add_file(self, file_path: str | Path) -> None:
-        file = File(file_path)
-        self.files.add(file)
-        self.backup(file)
+    def _process_directory(self, directory_path: Path, relative_path: Path = None) -> None:
+        if relative_path is None:
+            relative_path = Path()
 
-    def update(self) -> None:
-        for directory_path in self.directory.iterdir():
-            if directory_path.is_dir():
-                logging.info("Skipping directory: %s", directory_path.name)
-                continue
+        # Process directories first
+        for path in self.get_directories(directory_path):
+            new_relative_path = relative_path / path.name
+            self._process_directory(path, new_relative_path)
 
-            if File(directory_path) not in self.files:
-                self.add_file(directory_path)
-            else:
-                logging.info("Found: %s... Skipping", directory_path.name)
+        # Then process files
+        for path in self.get_files(directory_path):
+            self.file_handler.add_file(path, relative_path)
 
-    def backup(self, file: File) -> None:
-        if self.file_handler.needs_backup(file):
-            logging.info("Backing up: %s", file.file_path)
-            time_string = get_current_time_str()
-            logging.info("Current time string: %s", time_string)
-            backup_path = self.backup_folder / f"{file.file_path.stem}_{time_string}{file.file_path.suffix}"
-            logging.info(backup_path)
-            try:
-                shutil.copy(file.file_path, backup_path)
-                logging.info("Backup successful: %s", backup_path)
-            except FileNotFoundError:
-                logging.error("File not found: %s", file.file_path)
-            except PermissionError:
-                logging.error("Permission denied: %s", file.file_path)
-            except IOError:
-                logging.error("IO error occurred while backing up: %s", file.file_path)
-        else:
-            logging.info("No changes in: %s. Skipping backup.", file.file_path)
+    @staticmethod
+    def get_directories(directory_path: Path) -> Iterator[Path]:
+        return (p for p in directory_path.iterdir() if p.is_dir() and p.name != ".backup")
+
+    @staticmethod
+    def get_files(directory_path: Path) -> Iterator[Path]:
+        return (p for p in directory_path.iterdir() if not p.is_dir())
--- source/file_handler.py
+++ source/file_handler.py
@@ -1,21 +1,53 @@
+import logging
 import re
+import shutil
 from pathlib import Path
+from typing import Set
 
 from source.file import File
-from source.utils import timestamp_string_to_unix_float
+from source.utils import get_current_time_str, timestamp_string_to_unix_float
 
 
 class FileHandler:
     def __init__(self, backup_folder: Path) -> None:
         self.backup_folder = backup_folder
+        self.files: Set[File] = set()
+        self.timestamp_pattern = re.compile(r"_(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})")
 
     def get_latest_backup_time(self, file: File) -> str:
         backup_files = self.backup_folder.glob(f"{file.file_path.stem}_*{file.file_path.suffix}")
 
-        pattern = r"_(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})"
-        timestamps = [re.search(pattern, str(f)).group(1) for f in backup_files if re.search(pattern, str(f))]
+        timestamps = []
+        for file in backup_files:
+            match = self.timestamp_pattern.search(str(file))
+            if match:
+                timestamps.append(match.group(1))
 
         return max(timestamps, default="")
 
     def needs_backup(self, file: File) -> bool:
         return file.last_updated > timestamp_string_to_unix_float(self.get_latest_backup_time(file))
+
+    def add_file(self, file_path: str | Path, relative_path: str) -> None:
+        file = File(file_path)
+        self.files.add(file)
+        self.backup(file, relative_path)
+
+    def backup(self, file: File, relative_path: str) -> None:
+        if self.needs_backup(file):
+            time_string = get_current_time_str()
+            logging.info("Backing up: %s", file.file_path)
+            logging.info("Current time string: %s", time_string)
+            backup_path = self.backup_folder / relative_path / f"{file.file_path.stem}_{time_string}{file.file_path.suffix}"
+            backup_path.parent.mkdir(parents=True, exist_ok=True)
+            try:
+                shutil.copy(file.file_path, backup_path)
+                logging.info("Backup successful: %s", backup_path)
+            except FileNotFoundError:
+                logging.error("File not found: %s", file.file_path)
+            except PermissionError:
+                logging.error("Permission denied: %s", file.file_path)
+            except IOError:
+                logging.error("IO error occurred while backing up: %s", file.file_path)
+        else:
+            logging.info("No changes in: %s. Skipping backup.", file.file_path)
