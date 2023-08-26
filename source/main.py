a14178f4cc2adf212f0cfe4e99a58ab7d5b7b693
Douglas Monsky <douglas.monsky@gmail.com>
2023-08-25 20:10:59 -0400

    feat(main.py): refactor working folder to VcsDirectory and add CLI argparse
    This commit refactors the concept of a 'working folder' to a 'VcsDirectory' in
    the main.py file. It also introduces argparse to handle command line arguments
    for the path of the working directory and the log level. The function
    'create_working_folder' is renamed to 'create_vcs_root_directory' and its
    functionality is updated accordingly. The 'main' function is also updated to
    create a VcsDirectory instead of a working folder. The logging setup is moved
    inside the main function to allow for log level configuration via command line
    arguments.

--- source/main.py
+++ source/main.py
@@ -1,36 +1,37 @@
 """This module contains the main entry point for the application.
 
-It sets up logging, defines the default paths for the application, and provides functions for creating the working folder and running the application.
+It sets up logging, defines the default paths for the application, and provides functions for creating the VcsDirectory and running the application.
 
 Constants
 ---------
 CURRENT_DIRECTORY : Path
     The directory containing this script. It's set to the parent of the current file's directory.
-DEFAULT_WORKINGFOLDER_PATH : Path
-    The default path where the application will create its working folder. It's set to a 'WorkingFolder' directory in the same directory as this script.
+DEFAULT_VCS_DIR_PATH : Path
+    The default path where the application will create its VcsDirectory. It's set to a 'VcsDirectory' directory in the same directory as this script.
 
 Functions
 ---------
 get_home_relative_path() -> Path:
     Returns the home directory relative path.
-create_working_folder(path: Path = None) -> Path:
-    Creates a working folder at the given path. If no path is provided, uses the default working folder path.
+create_vcs_root_directory(path: Path = None) -> Path:
+    Creates a VcsDirectory at the given path. If no path is provided, uses the default VcsDirectory path.
 main(app_path: Path | str = None) -> None:
-    Starts the application, creates the working folder if it doesn't exist, and then closes the application.
+    Starts the application, creates the VcsDirectory if it doesn't exist, and then closes the application.
 """
+import argparse
 import logging
 from pathlib import Path
 
 from app import Application
 
-logging.basicConfig(level=logging.INFO)
-
 # Set the default path relative to the current script's directory, as to use the directory its contained within.
 CURRENT_DIRECTORY = Path(__file__).parent.parent
-DEFAULT_WORKINGFOLDER_PATH = CURRENT_DIRECTORY / 'WorkingFolder'
+DEFAULT_VCS_FOLDER_NAME = 'VcsDirectory'
+DEFAULT_VCS_DIR_PATH = CURRENT_DIRECTORY / DEFAULT_VCS_FOLDER_NAME
+DEFAULT_LOG_LEVEL = logging.INFO
 
 
-def get_home_relative_path() -> Path:
+def get_home_relative_path(folder_name: str = None) -> Path:
     """Returns the home directory relative path.
 
     Returns
@@ -38,32 +39,34 @@ def get_home_relative_path() -> Path:
     Path
         The home directory relative path.
     """
-    return Path.home() / 'WorkingFolder'
+    if folder_name is None:
+        folder_name = DEFAULT_VCS_FOLDER_NAME
+    return Path.home() / folder_name
 
 
-def create_working_folder(path: Path = None) -> Path:
-    """Creates a working folder at the given path. If no path is provided, uses the
-    default working folder path.
+def create_vcs_root_directory(path: Path = None) -> Path:
+    """Creates a VcsDirectory at the given path. If no path is provided, uses the
+    default VcsDirectory path.
 
     Parameters
     ----------
     path : Path, optional
-        The path where to create the working folder (default is None).
+        The path where to create the VcsDirectory (default is None).
 
     Returns
     -------
     Path
-        The path of the created working folder.
+        The path of the created VcsDirectory.
     """
     if path is None:
-        path = DEFAULT_WORKINGFOLDER_PATH
+        path = DEFAULT_VCS_DIR_PATH
         logging.info("Using default path: %s", path)
     path.mkdir(exist_ok=True)
     return path
 
 
 def main(app_path: Path | str = None) -> None:
-    """Starts the application, creates the working folder if it doesn't exist. Then The
+    """Starts the application, creates the backup folder if it doesn't exist. Then the
     application creates copies of files following the same directory structure within a
     .backup folder. Each copy is timestamped and appended to the end of the original
     filename in the format %Y-%m-%d_%H-%M-%S.
@@ -75,17 +78,22 @@ def main(app_path: Path | str = None) -> None:
     """
     logging.info("Starting application")
     app_path = Path(app_path) if app_path else None
-    # Creates the working folder if it doesn't exist, continues if it does.
-    app_path = create_working_folder(app_path)
+    # Creates the VcsDirectory if it doesn't exist, continues if it does.
+    app_path = create_vcs_root_directory(app_path)
     application = Application(app_path)
     application.update()
     logging.info("Closing application")
 
 
 if __name__ == "__main__":
-    # This will use the script's relative path by default.
-    main()
+    parser = argparse.ArgumentParser(description='Run the application.')
+    # Use . for the current directory the CLI is run from
+    parser.add_argument('--path', type=str, default=DEFAULT_VCS_DIR_PATH, help="The path of the working directory or 'script' for script directory")
+    parser.add_argument('--log', type=str, default=DEFAULT_LOG_LEVEL, help='Set the log level')
+    args = parser.parse_args()
 
-    # If you want to use the home directory relative path, you can call:
-    # home_path = get_home_relative_path()
-    # main(home_path)
+    log_level = logging.getLevelName(args.log)
+    logging.basicConfig(level=log_level)
+
+    path = args.path
+    main(path)
